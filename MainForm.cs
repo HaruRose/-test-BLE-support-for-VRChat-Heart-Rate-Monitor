cf6db295cb1dcee0eb6fa3f20a9f15f41106c113
HaruRose <161178913+HaruRose@users.noreply.github.com>
2025-04-04 23:20:14 +0300

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -316,7 +316,6 @@ namespace VRChatHeartRateMonitor
                 _deviceHandler = new DeviceHandler();
             }
 
-            // Common initialization for both handlers
             _deviceHandler.AdapterError += DeviceManager_AdapterError;
             _deviceHandler.DeviceFound += DeviceManager_DeviceFound;
             _deviceHandler.DeviceConnecting += DeviceManager_DeviceConnecting;
