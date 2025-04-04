71e33d47c35b3ab994e6cf662ad21f6924f90ff1
HaruRose <161178913+HaruRose@users.noreply.github.com>
2025-04-05 00:14:17 +0300

    Create IDeviceHandler.cs for handing choosing between BLE and BA

---
+++ IDeviceHandler.cs
@@ -0,0 +1,29 @@
+/// <summary>
+///  To resolve this error, ensure that BleHandler implements or inherits from DeviceHandler.
+/// </summary>
+using System;
+
+namespace VRChatHeartRateMonitor
+{
+    public interface IDeviceHandler
+    {
+        event Action AdapterError;
+        event Action<ulong, string> DeviceFound;
+        event Action DeviceConnecting;
+        event Action<ulong> DeviceConnected;
+        event Action DeviceDisconnecting;
+        event Action DeviceDisconnected;
+        event Action<string> DeviceError;
+        event Action<ushort> HeartRateUpdated;
+        event Action<ushort> BatteryLevelUpdated;
+
+        void StartScanning();
+        void StopScanning();
+        void SubscribeToDevice(ulong bluetoothDeviceAddress);
+        void UnsubscribeFromDevice();
+        ushort GetHeartRate();
+        bool IsListening();
+        bool CanConnect();
+        string BluetoothAddressToString(ulong bluetoothDeviceAddress);
+    }
+}
